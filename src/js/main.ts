ad68a75fb2d7b99227f0178457650dbbc012bc68
Nikola Stamatovic <stamatmail@gmail.com>
2021-09-20 14:45:44 +0200

    sticky state observer
    waiting for that :stuck pseudo

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,1 +1,14 @@
 console.log('hello 🜍')
+
+function stickyStateObserver(selector: string): void {
+  const elems:NodeListOf<HTMLElement> = document.querySelectorAll(selector);
+  const observer:IntersectionObserver = new IntersectionObserver(
+    ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),
+    { threshold: [1] }
+  );
+  elems.forEach((el) => {
+    el.style.top = '-1px';
+    observer.observe(el);
+  });
+}
+stickyStateObserver('.position-sticky');
