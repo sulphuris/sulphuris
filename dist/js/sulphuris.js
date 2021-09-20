9cc36d921287f66cf4b972dab2a2f6618b5a2e57
Nikola Stamatovic <stamatmail@gmail.com>
2021-09-20 14:50:06 +0200

    compile

--- dist/js/sulphuris.js
+++ dist/js/sulphuris.js
@@ -1,6 +1,18 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	console.log('hello 🜍');
+    console.log('hello 🜍');
+    function stickyStateObserver(selector) {
+        var elems = document.querySelectorAll(selector);
+        var observer = new IntersectionObserver(function (_a) {
+            var e = _a[0];
+            return e.target.classList.toggle('stuck', e.intersectionRatio < 1);
+        }, { threshold: [1] });
+        elems.forEach(function (el) {
+            el.style.top = '-1px';
+            observer.observe(el);
+        });
+    }
+    stickyStateObserver('.position-sticky');
 
 }());
